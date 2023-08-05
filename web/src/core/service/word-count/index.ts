4f4a9503d2d8b9a9d4f5861a9aca71bd5d0a9102
tonyzhou1890 <1332988826@qq.com>
2023-08-05 12:03:24 +0800

    refactor(word-count): tabList

--- web/src/core/service/word-count/index.ts
+++ web/src/core/service/word-count/index.ts
@@ -1,5 +1,137 @@
-import utils from './utils';
+import io from '@/core/io';
 
-export default {
-  ...utils,
-};
+let lemmatizer: any = null;
+
+// 下载脚本，初始化实例
+async function init() {
+  // 加载 underscore。因为 underscore 以 _ 作为命名空间，其他库也可能用这个符号，所以直接加载覆盖。
+  const underscoreSrc = '/libs/underscore/underscore-min.js';
+  await io.loadScript({
+    path: underscoreSrc,
+    version: '1.7.0',
+  });
+  if (!lemmatizer) {
+    const src = '/libs/lemmatizer/lemmatizer.js';
+    await io.loadScript({
+      path: src,
+      version: '0.0.2',
+    });
+  }
+  lemmatizer = new Lemmatizer('/libs/lemmatizer/');
+}
+
+export interface WordCountItem {
+  index?: number;
+  word: string;
+  count: number;
+  lang: string;
+}
+
+// 统计
+function count(data: string): WordCountItem[] {
+  type NumberObj = { [x: string]: number };
+
+  // 英语统计
+  const enData = data.replace(/\W+/g, '\n').toLowerCase();
+  const enRes: NumberObj = _count(
+    enData.split('\n').filter((v: string) => v),
+    'en'
+  );
+  // 中文统计
+  const zhData = data.replace(/[^\u4e00-\u9fa5]/g, '');
+  const zhRes: NumberObj = _count(zhData.split(''), 'zh');
+
+  let res: WordCountItem[] = [];
+
+  res = res.concat(
+    Object.entries(enRes).map((item) => ({
+      word: item[0],
+      count: item[1],
+      lang: 'en',
+    }))
+  );
+  res = res.concat(
+    Object.entries(zhRes).map((item) => ({
+      word: item[0],
+      count: item[1],
+      lang: 'zh',
+    }))
+  );
+
+  res.sort((a, b) => b.count - a.count);
+
+  return res;
+
+  function _count(arr: string[], lang: string): NumberObj {
+    const res: { [x: string]: number } = {};
+    for (let i = 0, len = arr.length; i < len; i++) {
+      const word = arr[i];
+      if (lang === 'en') {
+        const lemmasResult = lemmatizer.lemmas(word);
+        lemmasResult
+          .map((item: [string, string]) => item[0])
+          .filter((v: string) => v)
+          .map((item: string) => {
+            res[item] = (res[item] || 0) + 1;
+          });
+      } else {
+        res[word] = (res[word] || 0) + 1;
+      }
+    }
+
+    return res;
+  }
+}
+
+export type GroupedWordData = {
+    name: string;
+    value: string;
+    list: WordCountItem[];
+  }[]
+
+// 获取分类数据
+function listGrouping(list: WordCountItem[], lang?: string): GroupedWordData {
+  let tabs: { name: string; value: string }[] = [];
+  if (lang && lang !== 'all') {
+    tabs = [
+      {
+        name: `wordCount.${lang}`,
+        value: lang,
+      },
+    ];
+  } else {
+    tabs = [
+      {
+        name: 'wordCount.all',
+        value: 'all',
+      },
+      {
+        name: 'wordCount.zh',
+        value: 'zh',
+      },
+      {
+        name: 'wordCount.en',
+        value: 'en',
+      },
+    ];
+  }
+  return tabs.map(item => {
+    return {
+      ...item,
+      list: list.filter((word) => {
+        if (item.value === 'all') {
+          return true
+        }
+        return word.lang === item.value
+      }).map((item, index) => {
+        item.index = index + 1
+        return item
+      })
+    }
+  })
+}
+
+export default { init, count, listGrouping };
+
+export type Init = typeof init;
+export type Count = typeof count;
--- web/src/pages/word-count/IndexPage.vue
+++ web/src/pages/word-count/IndexPage.vue
@@ -1,25 +1,39 @@
 <template>
   <q-page>
     <div class="page-bg">
-      <ScatterIconsBackground :icons="backgroundIcons" color="gray" />
+      <ScatterIconsBackground
+        :icons="backgroundIcons"
+        color="gray"
+      />
     </div>
     <div class="page-main app">
-
       <div class="page-title tac">
         <h1 class="title">{{ $t('wordCount.title') }}</h1>
       </div>
       <div class="content">
         <p class="desc">{{ $t('wordCount.desc') }}</p>
         <div class="file">
-          <q-file v-model="file" :label="$t('wordCount.fileLabel')" accept=".txt" />
+          <q-file
+            v-model="file"
+            :label="$t('wordCount.fileLabel')"
+            accept=".txt"
+          />
         </div>
-        <div class="full-width" v-show="file">
+        <div
+          class="full-width"
+          v-show="file"
+        >
           <!-- file info -->
           <section class="info-section">
             <h2 class="section-title">{{ $t('wordCount.fileInfo') }}</h2>
             <q-list>
-              <q-item v-for="item in fileInfo" :key="item._id">
-                <q-item-section class="text-bold">{{ item.name }}</q-item-section>
+              <q-item
+                v-for="item in fileInfo"
+                :key="item._id"
+              >
+                <q-item-section class="text-bold">{{
+                  item.name
+                }}</q-item-section>
                 <q-item-section>{{ item.value }}</q-item-section>
               </q-item>
             </q-list>
@@ -27,53 +41,39 @@
           <!-- word fre list -->
           <section class="fre-section">
             <h2 class="section-title">{{ $t('wordCount.freSection') }}</h2>
-            <q-tabs v-model="currTab" class="text-teal">
-              <q-tab v-for="tab in tabList" :key="tab.value" :name="tab.value" :label="tab.name" />
+            <q-tabs
+              v-model="currTab"
+              class="text-teal"
+            >
+              <q-tab
+                v-for="tab in tabList"
+                :key="tab.value"
+                :name="tab.value"
+                :label="tab.name"
+              />
             </q-tabs>
-            <q-tab-panels v-model="currTab" animated swipeable transition-prev="jump-up" transition-next="jump-up">
-              <q-tab-panel v-for="tab in tabList" :key="tab.value" :name="tab.value">
-                <!-- <q-virtual-scroll style="max-height: 80vh;" :items="tab.list" separator>
-                  <template v-slot:before>
-                    <q-item dense>
-                      <q-item-section>
-                        <q-item-label class="text-weight-bold">
-                          {{ $t('wordCount.freListIndex') }}
-                        </q-item-label>
-                      </q-item-section>
-                      <q-item-section>
-                        <q-item-label class="text-weight-bold">
-                          {{ $t('wordCount.freListWord') }}
-                        </q-item-label>
-                      </q-item-section>
-                      <q-item-section>
-                        <q-item-label class="text-weight-bold">
-                          {{ $t('wordCount.freListCount') }}
-                        </q-item-label>
-                      </q-item-section>
-                    </q-item>
-                  </template>
-                  <template v-slot="{ item, index }">
-                    <q-item :key="index" dense>
-                      <q-item-section>
-                        <q-item-label>
-                          #{{ index + 1 }}
-                        </q-item-label>
-                      </q-item-section>
-                      <q-item-section>
-                        <q-item-label>
-                          {{ item.word }}
-                        </q-item-label>
-                      </q-item-section>
-                      <q-item-section>
-                        <q-item-label>
-                          {{ item.count }}
-                        </q-item-label>
-                      </q-item-section>
-                    </q-item>
-                  </template>
-                </q-virtual-scroll> -->
-                <q-table virtual-scroll flat bordered :virtual-scroll-sticky-size-start="48" row-key="index"
-                  :rows="tab.list" :columns="columns" :rows-per-page-options="[10, 20, 30, 50, 100, 0]" />
+            <q-tab-panels
+              v-model="currTab"
+              animated
+              swipeable
+              transition-prev="jump-up"
+              transition-next="jump-up"
+            >
+              <q-tab-panel
+                v-for="tab in tabList"
+                :key="tab.value"
+                :name="tab.value"
+              >
+                <q-table
+                  virtual-scroll
+                  flat
+                  bordered
+                  :virtual-scroll-sticky-size-start="48"
+                  row-key="index"
+                  :rows="tab.list"
+                  :columns="columns"
+                  :rows-per-page-options="[10, 20, 30, 50, 100, 0]"
+                />
               </q-tab-panel>
             </q-tab-panels>
           </section>
@@ -84,39 +84,35 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
-import wordCountService from '@/core/service/word-count'
-import type { WordCountItem } from '@/core/service/word-count/utils'
-import type { QTableProps } from 'quasar'
-import { useMeta } from 'quasar';
+import { computed, ref, shallowRef, watch } from 'vue';
+import wordCountService from '@/core/service/word-count';
+import type { WordCountItem, GroupedWordData } from '@/core/service/word-count';
+import type { QTableProps } from 'quasar';
 import { useI18n } from 'vue-i18n';
 
-const { t } = useI18n()
-
-useMeta({
-  title: `${t('wordCount.title')} | ${t('global.title')}`
-})
+const { t } = useI18n();
 
 // background
-const backgroundIcons = [{
-  name: 'snow',
-  count: 200,
-  minWidth: 16,
-  maxWidth: 30
-}].map
+const backgroundIcons = [
+  {
+    name: 'snow',
+    count: 200,
+    minWidth: 16,
+    maxWidth: 30,
+  },
+];
 
-const file = ref<null | File>(null)
-const fileText = ref('')
-const statisticList = ref<WordCountItem[]>([])
+const file = ref<null | File>(null);
+const fileText = ref('');
+let wordCountList = shallowRef<WordCountItem[]>([]);
 
 watch(file, async (newValue: File | null) => {
-  console.log(newValue)
   if (newValue) {
-    fileText.value = await newValue.text()
-    await wordCountService.init()
-    statisticList.value = wordCountService.count(fileText.value)
+    fileText.value = await newValue.text();
+    await wordCountService.init();
+    wordCountList.value = wordCountService.count(fileText.value);
   }
-})
+});
 
 // 文本信息
 const fileInfo = computed(() => {
@@ -124,84 +120,59 @@ const fileInfo = computed(() => {
     {
       name: t('wordCount.fileName'),
       value: file.value?.name ?? '',
-      _id: 0
+      _id: 0,
     },
     {
       name: t('wordCount.fileSize'),
-      value: ((file.value?.size ?? 0) / 1024).toFixed(2) + 'kb'
+      value: ((file.value?.size ?? 0) / 1024).toFixed(2) + 'kb',
     },
     {
       name: t('wordCount.textLength'),
-      value: fileText.value.length
+      value: fileText.value.length,
     },
   ].map((item) => {
-    item._id = Math.random()
-    return item
-  })
-})
+    item._id = Math.random();
+    return item;
+  });
+});
 
 // 当前 tab
-const currTab = ref('all')
+const currTab = ref('all');
 // tab 列表
-const tabList = computed<
-  { name: string, value: string, list: WordCountItem[] }[]
->(() => {
-  return [
-    {
-      name: t('wordCount.all'),
-      value: 'all',
-      list: [],
-    },
-    {
-      name: t('wordCount.zh'),
-      value: 'zh'
-    },
-    {
-      name: t('wordCount.en'),
-      value: 'en'
-    }
-  ].map(item => {
+const tabList = computed<GroupedWordData>(() => {
+  const groupedData = wordCountService.listGrouping(wordCountList.value);
+  return groupedData.map((item) => {
     return {
       ...item,
-      list: statisticList.value.filter((word) => {
-        if (item.value === 'all') {
-          return true
-        }
-        return word.lang === item.value
-      }).map((item, index) => {
-        item.index = index + 1
-        return item
-      })
-    }
-  })
-})
+      name: t(item.name),
+    };
+  });
+});
 
 const columns = computed<QTableProps['columns']>(() => [
   {
     name: 'index',
     label: t('wordCount.freListIndex'),
     field: 'index',
-    align: 'center'
+    align: 'center',
   },
   {
     name: 'word',
     label: t('wordCount.freListWord'),
     field: 'word',
-    align: 'center'
+    align: 'center',
   },
   {
     name: 'count',
     label: t('wordCount.freListCount'),
     field: 'count',
-    align: 'center'
-  }
-])
-
+    align: 'center',
+  },
+]);
 </script>
 
 <style lang="scss">
 .page-main {
-
   .file,
   .glyph-select {
     width: 300px;
