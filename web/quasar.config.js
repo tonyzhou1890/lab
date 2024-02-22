6c88bb51ab047b61733f01eaf339da797e997db9
周喜涛 <zhouxitao@zhouxitaodeMacBook-Pro.local>
2024-02-22 14:04:21 +0800

    chore: app-id 修改

--- web/quasar.config.js
+++ web/quasar.config.js
@@ -240,7 +240,7 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'instant-lab',
+        appId: 'snow-lab',
       },
     },
 
